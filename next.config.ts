7d75da23b6b099ba143802dd75ab1933addf465f
MateoYa <mateo.e.yajure@gmail.com>
2025-01-12 12:15:18 -0500

    more stuff

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    dirs: [], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    ignoreDuringBuilds: true,
+    
+  },
+  typescript:{
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
